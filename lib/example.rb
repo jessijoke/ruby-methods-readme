733edce7657afbd1b923a92235b376db1faadbc1
jessijoke <jessijoke@yahoo.com>
2020-10-19 01:12:21 -0400

    Done.

--- lib/example.rb
+++ lib/example.rb
@@ -1,6 +1,6 @@
 # Use this space to code along with the readme
 
-phrase = "Hello World!"
+phrase = "Hello World! one"
 puts phrase
 puts phrase
 puts phrase
@@ -11,3 +11,35 @@ puts phrase
 puts phrase
 puts phrase
 puts phrase
+
+def say_hello_world_ten_times
+    10.times { puts "Hello World! two"}
+end
+
+say_hello_world_ten_times
+
+def greeting
+    phrase = "Hello World! two.5"
+    puts phrase
+    puts phrase
+    puts phrase
+    puts phrase
+    puts phrase
+    puts phrase
+    puts phrase
+    puts phrase
+    puts phrase
+    puts phrase
+end
+
+greeting
+
+def greet_loop
+    i = 0
+    while i < 10
+        i += 1
+        puts "Hello World! three"
+    end
+end
+
+greet_loop
